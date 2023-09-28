8523ea01d732b5572449956e78df955eb3adbf97
André Caracioly <andrecaracioly@gmail.com>
2023-09-28 15:05:55 -0300

    buttons design Ok

--- app/src/main/java/com/example/calculadora_mobile/MainActivity.kt
+++ app/src/main/java/com/example/calculadora_mobile/MainActivity.kt
@@ -2,10 +2,13 @@ package com.example.calculadora_mobile
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
     }
 }
